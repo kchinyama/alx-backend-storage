7d7652cc0e7a4c316160e408505cf51a2931ddf5
root <kaumba@kalichipictures.com>
2024-10-23 08:33:07 -0700

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@ demo script of redis with python
 
 
 import redis
-from typing import Any, Union
+from typing import Any, Union, Optional, Callable
 import uuid
 
 
@@ -25,3 +25,22 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str,
+            byte, int, float, None]:
+        """method converts byte to original format using the key"""
+
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """auto paramatises Caache.get with correct conversion function"""
+
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """auto paramatises Caache.get with correct conversion function"""
+
+        return self.get(key, int)
